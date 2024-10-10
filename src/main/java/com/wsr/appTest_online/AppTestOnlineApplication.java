aba36425e3c50044ee6f9575a5bf4b9c7cb68ebc
DeveloperOak <wsandovalrosas@gmail.com>
2024-10-09 21:33:13 -0500

    Agregando proyecto Spring Initializ

---
+++ src/main/java/com/wsr/appTest_online/AppTestOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.wsr.appTest_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTestOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTestOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsr/appTest_online/AppTestOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsr.appTest_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTestOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
